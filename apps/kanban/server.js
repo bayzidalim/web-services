50a316fff79459a482cd11a7f7e6989025aa76c0
Bayzid Alim <bayzidalim@gmail.com>
2026-02-02 20:09:22 +0600

    Fix: Allow wildcard CORS in Kanban

--- apps/kanban/server.js
+++ apps/kanban/server.js
@@ -19,7 +19,7 @@ const allowedOrigins = [
 
 app.use(cors({
   origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
+    if (!origin || allowedOrigins.includes(origin) || allowedOrigins.includes('*')) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
